c906451db150836dd6892211e6fe7c36fa82da76
Therealkos <153295572+Therealkosbruh@users.noreply.github.com>
2024-09-04 22:07:21 +0300

    Update function.mjs added language parametr

--- function.mjs
+++ function.mjs
@@ -3,14 +3,17 @@ import OpenAI from "openai";
 const apiKey = "";
 const openai = new OpenAI({ apiKey: apiKey });
 
-async function main(url) {
+async function main(url, language) {
   const response = await openai.chat.completions.create({
     model: "gpt-4o-mini",
     messages: [
       {
         role: "user",
         content: [
-          { type: "text", text: "You are an advanced assistant for recognizing objects in a photo. you recognize the main object in the photo and write what it is. you can recognize absolutely any object or thing. Detailed exceptions are listed below: if you recognize a car, specify its make and model. if the model could not be recognized, specify its brand. if you recognize the flowers, specify the name of the flower. if you recognize a dish, specify its name as accurately as possible (for example, the name of the pizza, the variety of apples, berries, and so on). if you recognize a toy (children's), specify its name in as much detail as possible. if you recognize the book, specify its author and title if you recognize a person or a face, if it is a famous person, specify his first and last name, if it is an ordinary person, specify a girl / man , if you recognize a technique, specify its model and manufacturer, if possible, always specify the model of the product and its manufacturer, if this product is popular (except for food), answers they should be short (one or two words, or the name of the product). full name of the model) You do not need to describe the color of the product or its detailed characteristics. You only need the name/modelthe response format without clarifying words (without 'this, in the photo, here' and so on) is just the name of the object or mode" },
+          {
+            type: "text",
+            text: `You are an advanced assistant for recognizing objects in a photo. You recognize the main object in the photo and write what it is. You can recognize absolutely any object or thing. Detailed exceptions are listed below: if you recognize a car, specify its make and model. if the model could not be recognized, specify its brand. if you recognize the flowers, specify the name of the flower. if you recognize a dish, specify its name as accurately as possible (for example, the name of the pizza, the variety of apples, berries, and so on). if you recognize a toy (children's), specify its name in as much detail as possible. if you recognize the book, specify its author and title if you recognize a person or a face, if it is a famous person, specify his first and last name, if it is an ordinary person, specify a girl / man , if you recognize a technique, specify its model and manufacturer, if possible, always specify the model of the product and its manufacturer, if this product is popular (except for food), answers they should be short (one or two words, or the name of the product). full name of the model) You do not need to describe the color of the product or its detailed characteristics. You only need the name/modelthe response format without clarifying words (without 'this, in the photo, here' and so on) is just the name of the object or mode`,
+          },
           {
             type: "image_url",
             image_url: {
@@ -37,7 +40,7 @@ async function main(url) {
         description: "...",
         facts: ,
           ...
-        Ещё в параметр labels добавьте только название модели Пожалуйста, включите технические характеристики для электроники, ингредиенты для продуктов питания и другие соответствующие детали. Используйте точные значения, если это возможно. Если значения варьируются, то выводите только усредненное значение, если значение имеет хоть какой то числовой формат выводите только число. Так же если в ответе присутствует цена, то выводите конкретное значение цены и валюту, если присутствуют какие-то технические показатели выводите только их, без описания, если есть вес предмета тоже выводите только число, так же ко всему остальному, где используются числовые значения.В ответе используй только указанный формат, не нужно любезно отвечать на мой запрос и писать что то кроме ответа по типу :"Конечно" и так далее. Так же верни результат в формате русской раскладки`,
+        Ещё в параметр labels добавьте только название модели Пожалуйста, включите технические характеристики для электроники, ингредиенты для продуктов питания и другие соответствующие детали. Используйте точные значения, если это возможно. Если значения варьируются, то выводите только усредненное значение, если значение имеет хоть какой то числовой формат выводите только число. Так же если в ответе присутствует цена, то выводите конкретное значение цены и валюту, если присутствуют какие-то технические показатели выводите только их, без описания, если есть вес предмета тоже выводите только число, так же ко всему остальному, где используются числовые значения.В ответе используй только указанный формат, не нужно любезно отвечать на мой запрос и писать что то кроме ответа по типу :"Конечно" и так далее. Так же верни результат в формате ${language} раскладки`,
       },
     ],
   });
@@ -83,7 +86,7 @@ resultArray.push({
   description,
   facts: facts.map(fact => fact.trim().charAt(0).toUpperCase() + fact.trim().slice(1).toLowerCase())
 });
-
+// console.log(resultArray);
 return resultArray;
 }
 
